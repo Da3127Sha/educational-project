96b6256ec0ef7a431cd7a8039ab4263b8136832d
DashaVasilyeva <da3127@yandex.ru>
2018-11-16 13:24:19 +0300

    moved tests to a separate directory - issue #4

--- README.md
+++ README.md
@@ -13,7 +13,10 @@ Educational project for collective application development
     + `prjadm.xml` - file with xml description of `PRJADM` database schema
     + `tasks.xml` - file with xml description of `TASKS` database schema
 + utils
-    + `XmlParser.py` - contains function `createObjectFromXml` which creates classes from xml description
+    + `XmlParser.py` - contains function `create_list_of_objects_from_xml` which creates classes from xml description
     + `DBInitializer` - class, which contains functions for database initialization
     + `RAMToDBDConverter.py` - contains functions for inserting into database tables
++ tests
+    + `RAMToDBDTests.py` - tests of RAM to DBD
+    + `XMLToRAMTests` - tests of XML to RAM
 + `MainApplication.py` - main application
--- tests/RAMToDBDTests.py
+++ tests/RAMToDBDTests.py
@@ -3,9 +3,9 @@ from utils.DBInitializer import *
 from utils.RAMToDBDConverter import *
 import os
 
-DATABASE_NAME = "database.db"
-XML_FILE_NAME_1 = "./resources/tasks.xml"
-XML_FILE_NAME_2 = "./resources/prjadm.xml"
+DATABASE_NAME = "../database.db"
+XML_FILE_NAME_1 = "../resources/tasks.xml"
+XML_FILE_NAME_2 = "../resources/prjadm.xml"
 
 schemas = create_list_of_objects_from_xml([XML_FILE_NAME_1, XML_FILE_NAME_2])
 
@@ -16,7 +16,12 @@ if create:
     converter_to_database = RAMToDBDConverter(DATABASE_NAME)
     converter_to_database.RAM_to_DBD(schemas)
 
-
-
-
-
+print("-------------")
+connection = sqlite3.connect(DATABASE_NAME)
+cursor = connection.cursor()
+cursor.execute("pragma foreign_keys=on;")
+cursor.execute("""select * from dbd$domains """)
+records = cursor.fetchall()
+for i, record in enumerate(records):
+    print(record)
+cursor.close()
